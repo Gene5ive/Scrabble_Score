582d91056792763a528ab6496b5773d3b16e2b7b
Frank Johnson & Stephen Ajayi <student@epicodus.com>
2015-04-22 13:39:59 -0700

    takes a word or letter and returns a scrabble score + test. working.

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -1,8 +1,13 @@
 require('rspec')
 require('scrabble_score')
+require('pry')
 
 describe('String#scrabble_score') do
   it("takes a letter and returns a scrabble score") do
     expect("a".scrabble_score()).to(eql(1))
+  end
+
+  it("takes a word and returns a scrabble score") do
+    expect("ampersand".scrabble_score()).to(eql(14))
   end
 end

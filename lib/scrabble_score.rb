a4c3d65b2fca1e98573f9e913b641daf33c0773d
Frank Johnson <jedigene@gmail.com>
2015-04-24 17:00:04 -0700

    Added appropriate commenting to scrabble_score.rb

--- lib/scrabble_score.rb
+++ lib/scrabble_score.rb
@@ -1,7 +1,7 @@
 class String
-
+#establish a class object
   define_method(:scrabble_score) do
-
+#define a class method
     letter_options = {
       "a" => 1, "b" =>3, "c" => 3, "d" => 2, "e" => 1, "f" => 4,
       "g" => 2, "h" => 4, "i" => 1, "j" => 8, "k" => 5, "l"=> 1,
@@ -9,17 +9,18 @@ class String
       "s" => 1, "t" => 1, "u" => 1, "v" => 4, "w" => 4, "x" => 8,
       "y"=> 4, "z" => 10
     }
-
+#place every letter within a hash and its respective score within a hash
     word = self
-
+#idenify the input with a variable
     score = 0
-
+#establish an empty score counter
     letters = word.split("")
-
+#split the input into a useful array
     letters.each() do |letter|
       score += letter_options.fetch(letter)
     end
-
+#iterate through the array adding scores to caounter
     score
+#return score
   end
 end
